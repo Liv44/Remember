14cf23856d46e29d3193ee0df44bacd5b563e91f
Olivia Moreau <moreauolivia49@gmail.com>
2025-01-17 10:00:52 +0100

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Olivia Moreau on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
